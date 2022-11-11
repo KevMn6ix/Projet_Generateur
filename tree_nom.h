323f0cf97f857eda75fea860770c8a49358264fa
Aureldns <94120206+Aureldns@users.noreply.github.com>
2022-11-11 19:12:29 +0100

    Création des arbres n aires (verbes, noms, adjectifs) et génération des phrases de base
    Signed-off-by: Aureldns <94120206+Aureldns@users.noreply.github.com>

--- tree_nom.h
+++ tree_nom.h
@@ -4,8 +4,13 @@
 
 #ifndef PROJET_GENERATEUR_TREE_NOM_H
 #define PROJET_GENERATEUR_TREE_NOM_H
-#define ALPHABET 26
 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "txt_fonctions.h"
 
 typedef struct fl_nom
 {
@@ -21,24 +26,31 @@ typedef struct maillon_l_fl_nom
     struct maillon_fl_nom *next;
 } *p_fl_nom;
 
-typedef struct s_std_list
+typedef struct s_std_list_nom
 {
     p_fl_nom head;
 } l_fl_nom;
 
-typedef struct noeud_nom
+typedef struct s_letter_node_nom
 {
     char lettre;
     int nb_formes; // nombre formes_flechies
+    int end_word;
     l_fl_nom nom;
-    struct noeud_nom* sons[ALPHABET];
+    struct s_letter_node_nom* sons[ALPHABET];
 
-}noeud_nom;
+}t_letter_node_nom, * p_letter_node_nom;
 
 typedef struct arbre_verbe
 {
-    noeud_nom root;
-}tree_nom;
+    p_letter_node_nom root;
+}t_tree_nom;
+
+p_letter_node_nom createLetterNodeNom(char letter);
+t_tree_nom createEmptyTreeNom();
+
+t_tree_nom generateTreeNom(FILE* filename);
 
 
+void generateNom(t_tree_nom tree);
 #endif //PROJET_GENERATEUR_TREE_NOM_H
