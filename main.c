8efa3aae9d6a667a849085d0fe32199b15095872
Aureldns <94120206+Aureldns@users.noreply.github.com>
2022-11-11 19:06:06 +0100

    Création des arbres n aires (verbes, noms, adjectifs) et génération des phrases de base
    Signed-off-by: Aureldns <94120206+Aureldns@users.noreply.github.com>

--- main.c
+++ main.c
@@ -1,32 +1,21 @@
 #include <stdlib.h>
+#include <time.h>
 #include <string.h>
-#include <stdio.h>
+
+#include "txt_fonctions.h"
 #include "tree_verbe.h"
+#include "tree_nom.h"
+#include "tree_adject.h"
 
 
-int main() {
 
-    FILE *fic ;
-    fic = fopen("test.txt","r");
-    char texte[256];
 
-    while(fgets(texte, 255, fic)!= NULL)
-    {
-        char ligne[256];
-        strcpy(ligne,texte);
-        char d[] = "\t";
-        char *portion1 = strtok(texte,d);
-        char *portion2 = strtok(NULL,d);
-        char *portion3 = strtok(NULL,":");
+int main()
+{
+    srand(time(NULL));
 
-        if(strcmp(portion3,"Ver")==0)
-        {
-            printf("%s",ligne);
-            if(!isVrbInFichier(portion2))
-            {
-                ajt_vrb_txt(portion2);
-            }
-        }
-    }
+    generateBaseSentenceModel1("C:\\Users\\PC-Aurelien\\Desktop\\Projet_Generateur_Copy_version\\dictionnaire_non_accentue.txt");
+    printf("\n");
+    generateBaseSentenceModel2("C:\\Users\\PC-Aurelien\\Desktop\\Projet_Generateur_Copy_version\\dictionnaire_non_accentue.txt");
     return 0;
 }
