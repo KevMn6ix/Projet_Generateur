83af3a60df7cb1d0c8678a17a36fbd4ed00fc0c5
Aureldns <94120206+Aureldns@users.noreply.github.com>
2022-11-11 19:09:25 +0100

    Création des arbres n aires (verbes, noms, adjectifs) et génération des phrases de base
    Signed-off-by: Aureldns <94120206+Aureldns@users.noreply.github.com>

--- tree_adject.h
+++ tree_adject.h
@@ -4,7 +4,12 @@
 
 #ifndef PROJET_GENERATEUR_TREE_ADJECT_H
 #define PROJET_GENERATEUR_TREE_ADJECT_H
-#define ALPHABET 26
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "txt_fonctions.h"
 
 typedef struct fl_adject
 {
@@ -21,24 +26,31 @@ typedef struct maillon_l_fl_adject
     struct maillon_fl_adj *next;
 } *p_fl_adject;
 
-typedef struct s_std_list
+typedef struct s_std_list_adject
 {
     p_fl_adject head;
 } l_fl_adject;
 
-typedef struct noeud_adject
+typedef struct s_letter_node_vrb_adject
 {
     char lettre;
     int nb_formes; // nombre formes_flechies
+    int end_word;
     l_fl_adject adject;
-    struct noeud_adject* sons[ALPHABET];
+    struct s_letter_node_vrb_adject* sons[ALPHABET];
 
-}noeud_adject;
+}t_letter_node_adject, * p_letter_node_adject;
 
-typedef struct arbre_adject
+typedef struct s_tree_adject
 {
-    noeud_adject root;
-}tree_adject;
+    p_letter_node_adject root;
+}t_tree_adject;
 
+p_letter_node_adject createLetterNodeAdject(char letter);
+t_tree_adject createEmptyTreeAdject();
+
+t_tree_adject generateTreeAdject(FILE* filename);
+
+void generateAdject(t_tree_adject tree);
 
 #endif //PROJET_GENERATEUR_TREE_ADJECT_H
