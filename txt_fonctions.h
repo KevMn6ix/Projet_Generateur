86f97003996d6736484d92393a1005aeafb1ed2f
Aureldns <94120206+Aureldns@users.noreply.github.com>
2022-11-13 00:15:48 +0100

    Ajout des commentaires
    Signed-off-by: Aureldns <94120206+Aureldns@users.noreply.github.com>

--- txt_fonctions.h
+++ txt_fonctions.h
@@ -15,10 +15,12 @@
 #include "tree_nom.h"
 #include "tree_adject.h"
 
-
+//Cette variable permet de convertir un caractère en son numéro de l'alphabet à la même manière d'un chiffrement de Cesar
 int convertCharInInt(char letter);
 
+//Cette fonction permet de générer une phrase de base qui suit le modèle : nom - adjectif - verbe - nom
 void generateBaseSentenceModel1(FILE* filename);
+//Cette fonction permet de générer une phrase de base qui suit le modèle : nom - 'qui' - verbe - verbe - nom - adjectif
 void generateBaseSentenceModel2(FILE* filename);
 
 #endif //PROJET_GENERATEUR_TXT_FONCTIONS_H
