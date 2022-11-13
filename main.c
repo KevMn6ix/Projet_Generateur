8264a235868f976831929ab9383d0b5468779e3e
Aureldns <94120206+Aureldns@users.noreply.github.com>
2022-11-13 10:47:14 +0100

    Ajout du menu du projet
    Signed-off-by: Aureldns <94120206+Aureldns@users.noreply.github.com>

--- main.c
+++ main.c
@@ -1,21 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
 
 #include "txt_fonctions.h"
-#include "tree_verbe.h"
-#include "tree_nom.h"
-#include "tree_adject.h"
-
-
-
 
 int main()
 {
     srand(time(NULL));
 
-    generateBaseSentenceModel1("C:\\Users\\PC-Aurelien\\Desktop\\Projet_Generateur_Copy_version\\dictionnaire_non_accentue.txt");
-    printf("\n");
-    generateBaseSentenceModel2("C:\\Users\\PC-Aurelien\\Desktop\\Projet_Generateur_Copy_version\\dictionnaire_non_accentue.txt");
+    fonction_menu();
+
     return 0;
 }
